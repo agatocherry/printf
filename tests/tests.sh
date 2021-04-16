5d103bf07b87d12250986f6cb139aed98ee51315
agatocherry <agcolas@student.42.fr>
2021-04-16 18:45:24 +0200

    Update tests.yml and tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,7 +2,7 @@
 cd .. && Make && cd tests
 
 #Announce what type of test is being done
-echo -e "\033[3;37mSimple test (without any argument)\033[0m"
+echo "\033[3;37mSimple test (without any argument)\033[0m"
 
 #Compile the test
 gcc simple.c -D PRINT=printf && ./a.out > ./log/printf.log
@@ -12,14 +12,14 @@ diff ./log/printf.log ./log/ft_printf.log > ./log/diff.log
 #Check if the diff file is ok or not
 if [ -s ./log/diff.log ]
 then
-	echo -e "\033[1;31mK.O\033[2;37m" && cat ./log/diff.log
+	echo  "\033[1;31mK.O\033[2;37m" && cat ./log/diff.log
 	rm -rf a.out && rm -rf a.out.dSYM && cd .. && Make fclean && exit 1
 else
-	echo -e "\033[1;32mOK :D\033[0m"
+	echo "\033[1;32mOK :D\033[0m"
 fi
 
 #Announce what type of test is being done
-echo -e "\033[3;37mPercent test\033[0m"
+echo "\033[3;37mPercent test\033[0m"
 
 #Compile the test
 gcc percent.c -D PRINT=printf && ./a.out > ./log/printf.log
@@ -29,14 +29,14 @@ diff ./log/printf.log ./log/ft_printf.log > ./log/diff.log
 #Check if the diff file is ok or not
 if [ -s ./log/diff.log ]
 then
-	echo -e "\033[1;31mK.O\033[2;37m" && cat ./log/diff.log
+	echo "\033[1;31mK.O\033[2;37m" && cat ./log/diff.log
 	rm -rf a.out && rm -rf a.out.dSYM && cd .. && Make fclean && exit 1
 else
-	echo -e "\033[1;32mOK :D\033[0m"
+	echo "\033[1;32mOK :D\033[0m"
 fi
 
 #Announce what type of test is being done
-echo -e "\033[3;37mChar test\033[0m"
+echo "\033[3;37mChar test\033[0m"
 
 #Compile the test
 gcc char.c -D PRINT=printf && ./a.out > ./log/printf.log
@@ -46,10 +46,10 @@ diff ./log/printf.log ./log/ft_printf.log > ./log/diff.log
 #Check if the diff file is ok or not
 if [ -s ./log/diff.log ]
 then
-	echo -e "\033[1;31mK.O\033[2;37m" && cat ./log/diff.log
+	echo "\033[1;31mK.O\033[2;37m" && cat ./log/diff.log
 	rm -rf a.out && rm -rf a.out.dSYM && cd .. && Make fclean && exit 1
 else
-	echo -e "\033[1;32mOK :D\033[0m"
+	echo "\033[1;32mOK :D\033[0m"
 fi
 
 #Clean all
