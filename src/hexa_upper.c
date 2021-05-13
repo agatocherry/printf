0ef5239f06445659a156797f5163bdc958d36d67
agatocherry <agcolas@student.42.fr>
2021-05-13 18:03:18 +0200

    Update hexa
    Take the unsigned and put in in hexa and its work

--- src/hexa_upper.c
+++ src/hexa_upper.c
@@ -5,74 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: agcolas <agcolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/16 14:46:34 by agcolas           #+#    #+#             */
-/*   Updated: 2021/05/13 17:44:17 by agcolas          ###   ########.fr       */
+/*   Created: 2021/04/22 11:19:25 by agcolas           #+#    #+#             */
+/*   Updated: 2021/05/13 18:02:01 by agcolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/printf.h"
 
-static void	pre_pre_process(int *len, int *display, t_flags flags[4], unsigned int pointer)
+static void	process(t_flags flags[4], int len, int *display, int nb)
 {
-	if (flags[2].count != -1 && flags[3].count == 0 && flags[2].is_star == 0 && flags[3].is_star == 0 && pointer != 0)
-		flags[2].count -= 1;
-	if (flags[3].count == 0 && flags[0].count != 1 && flags[0].negative == 1
-	&& flags[0].is_star == 1 && pointer == 0)
-		flags[0].count--;
-	if (flags[2].count != -1 && flags[3].count == 0 && flags[3].is_star == 1)
-		if (pointer != 0)
-			flags[2].count -= 1;
-	if (flags[0].count != -1 && flags[0].negative == 1)
-	{
-		flags[1].count = flags[0].count;
-		flags[0].count = -1;
-	}
-	else if (flags[0].count != -1 && flags[0].negative == 1
-	&& flags[3].count != -1)
-	{
-		flags[1].count = flags[0].count;
-		flags[0].count = -1;
-		flags[3].count = -1;
-	}
-	if (flags[2].count != -1 && flags[2].negative == 1 && flags[3].count != 0)
-	{
-		flags[1].count = flags[2].count;
-		flags[2].count = -1;
-	}
-}
-
-static void	pre_process(int *len, int *display, t_flags flags[4], unsigned int pointer)
-{
-	int no_put = 0;
-	pre_pre_process(len, display, flags, pointer);
-	if (flags[2].count != -1 && flags[2].negative == 1)
-	{
-		flags[1].count = flags[2].count;
-		flags[2].count = -1;
-	}
-	if (flags[3].count != 1 && flags[3].negative == 1)
-		flags[3].count = -1;
-	if (flags[2].count != -1 && flags[3].count != -1 && flags[3].negative == 0)
-	{
-		if (flags[3].count == 0 && flags[2].negative == 0 && flags[3].is_star == 0 && flags[2].is_star == 1 && flags[2].count != -1 && pointer != 0)
-			no_put = 1;
-		flags[0].count = flags[2].count;
-		flags[2].count = -1;
-		if (flags[3].count == 0 && no_put == 0)
-			flags[0].count++;
-	}
-	no_put = 0;
-	if (flags[1].count != -1 && flags[3].count != -1 && flags[3].negative == 0 && flags[0].is_star == 1 && flags[3].is_star == 1)
-		flags[1].count++;
-	if (flags[2].count != -1 && flags[3].count != -1 && flags[3].negative == 1)
-		flags[3].count = -1;
-	if (flags[0].count != -1 && flags[3].count > 0 && flags[3].count < *len)
-		flags[0].count -= (*len - flags[3].count);
-}
-
-static void	process(t_flags flags[4], int len, int *display, int no_put)
-{
-	if (flags[3].count > 0)
+	if (flags[3].count > 0 || (nb == 0 && flags[3].count != -1))
 	{
 		flags[2].count = flags[3].count;
 		flags[3].count = -4;
@@ -98,50 +40,105 @@ static void	process(t_flags flags[4], int len, int *display, int no_put)
 	}
 }
 
-static void	end_process(t_flags flags[4], int len, int *display, int save)
+static void	pre_pre_process(int *len, int *display, t_flags flags[4], int *save)
 {
-	if (flags[3].count == -4 && save != 0)
+	pre_process_unsigned(len, display, flags, save);
+	if (flags[0].count != -1 && flags[3].count > 0 && flags[3].count < *len
+	&& flags[0].negative == 0)
+		flags[0].count -= (*len - flags[3].count);
+	if (flags[0].count != -1 && flags[3].count > flags[0].count
+	&& flags[3].count > *len)
+	{
+		flags[2].count = flags[3].count;
+		flags[3].count = -1;
+		flags[0].count = -1;
+	}
+	if (flags[0].count != -1 && flags[3].count > flags[0].count
+	&& flags[3].count > *len)
+	{
+		flags[2].count = flags[3].count;
+		flags[3].count = -1;
+		flags[0].count = -1;
+	}
+	if (flags[0].count != -1 && flags[3].count < flags[0].count
+	&& flags[3].count > *len && flags[0].negative == 1)
+	{
+		flags[2].count = flags[3].count;
+		flags[1].count -= flags[2].count;
+		flags[3].count = -1;
+	}
+}
+
+static void	pre_process(int *len, int *display, t_flags flags[4], int *save)
+{
+	pre_pre_process(len, display, flags, save);
+	if (flags[0].count != -1 && flags[3].count == flags[0].count
+	&& flags[3].count > *len && flags[0].negative == 1)
+	{
+		flags[2].count = flags[3].count;
+		flags[3].count = -1;
+		flags[0].count = -1;
+	}
+	if (flags[0].count != -1 && flags[0].negative == 1 && flags[3].count > 0)
+	{
+		flags[1].count = flags[0].count;
+		flags[0].count = -1;
+		flags[3].count = -1;
+		*save = -1;
+	}
+	else if (flags[0].count != -1 && flags[0].negative == 1)
+	{
+		flags[1].count = flags[0].count;
+		flags[0].count = -1;
+	}
+}
+
+static int	end_process(t_flags flags[4], int len, int save, int nb)
+{
+	int	display;
+
+	display = 0;
+	if (flags[1].count > save && save > 0 && len > save)
+		flags[1].count -= (len - save);
+	if (save > 0 || (nb == 0 && save != -1))
 		while (flags[1].count > save)
 		{
 			ft_putchar(' ');
-			*display += 1;
+			display += 1;
 			flags[1].count--;
 		}
 	else
 		while (flags[1].count > len)
 		{
 			ft_putchar(' ');
-			*display += 1;
+			display += 1;
 			flags[1].count--;
 		}
+	return (display);
 }
 
 void		argument_hexa_upper(int *display, va_list parameters,
 			t_flags flags[4])
 {
 	int				len;
-	unsigned int	pointer;
+	unsigned int	nb;
 	int				save;
 	int				no_put;
 
 	no_put = 0;
-	save = 0;
-	pointer = va_arg(parameters, unsigned int);
-	len = ft_hexalen((long)pointer);
-	if (pointer == 0)
+	save = flags[3].count;
+	nb = va_arg(parameters, unsigned int);
+	len = ft_hexalen(nb);
+	if (nb == 0)
 		len++;
-	if (pointer == 0 && flags[3].count == 0)
-		flags[0].count++;
-	if (pointer == 0 && flags[3].count == 0)
+	if (nb == 0 && flags[3].count == 0)
 		no_put = 1;
-	if (flags[3].count > len)
-		save = flags[3].count;
-	pre_process(&len, display, flags, pointer);
-	process(flags, len, display, no_put);
+	pre_process(&len, display, flags, &save);
+	process(flags, len, display, nb);
 	if (save != 0 || no_put == 0)
-		ft_puthexa_upper((long)pointer);
+		ft_puthexa_upper(nb);
 	if (no_put == 1)
 		len--;
-	end_process(flags, len, display, save);
+	*display += end_process(flags, len, save, nb);
 	*display += len;
 }
