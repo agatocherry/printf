1f446ad0cf9ca086de005f79a7433b85779c3c77
agatocherry <agcolas@student.42.fr>
2021-05-13 17:40:09 +0200

    Update printf
    Return when no cspdiux after a %

--- src/printf.c
+++ src/printf.c
@@ -6,7 +6,7 @@
 /*   By: agcolas <agcolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 15:25:39 by agcolas           #+#    #+#             */
-/*   Updated: 2021/05/13 12:09:01 by agcolas          ###   ########.fr       */
+/*   Updated: 2021/05/13 17:38:24 by agcolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,6 @@ static int	cspdiux(char c)
 		return (1);
 	return (0);
 }
-
-static void	no_cspdiux(t_flags flags[4], int *display, const char *str)
-{
-	if (*str)
-	{
-		while (flags[0].count > 1)
-		{
-			ft_putchar(' ');
-			*display += 1;
-			flags[0].count--;
-		}
-		while (flags[2].count > 1)
-		{
-			ft_putchar('0');
-			*display += 1;
-			flags[2].count--;
-		}
-		ft_putchar(*str);
-		*display += 1;
-		if (flags[1].count != -1)
-		{
-			while (flags[1].count > 1)
-			{
-				ft_putchar(' ');
-				*display += 1;
-				flags[1].count--;
-			}
-		}
-	}
-}
-
 
 int		ft_printf(const char *str, ...)
 {
@@ -64,7 +33,7 @@ int		ft_printf(const char *str, ...)
 		{
 			str = search_flags(str, &display, flags, parameters);
 			if (cspdiux(*str) != 1)
-				no_cspdiux(flags, &display, str);
+				return (display);
 		}
 		else
 		{
